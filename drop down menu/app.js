17767e4e2ba7052fa93b59cf76dfecd849f9bb50
djalabz <djalabz@gmail.com>
2025-01-29 16:25:10 +0100

    :sparkles: Correction du burger menu

--- drop down menu/app.js
+++ drop down menu/app.js
@@ -10,3 +10,36 @@
 
 // En revanche, un impératif, lorsque l'on clique une première fois le menu s'ouvre
 // Et lorsque l'on reclique il se ferme
+
+const burger = document.querySelector("#burger-logo")
+const menu = document.querySelector(".menu-wrapper")
+const links = menu.querySelectorAll("a")
+
+burger.addEventListener("click", () => {
+    // Si jamais mon menu est ouvert alors on le cache vers la droite et on réduit l'opacité de nos lien à 0
+    if (menu.style.right === "0px") {
+        // Repositionnement du menu wrapper vers la droite 
+        menu.style.right = "-100vw"
+
+        // On transforme notre links (objet) en tableau avant de boucler 
+        // Pouyr chaque lien je veux réduire son opacité 
+        Array.from(links).forEach((link, index) => {
+            setTimeout(() => {
+                link.style.opacity = "0"
+            }, 200 * index)
+        })
+    // Dans les autres cas on affiche le menu et les liens de manière progressive 
+    } else {
+        // On affiche le menu en précisant right = 0
+        menu.style.right = "0px";
+
+        // Pour chaque lien de links je les fait apparaitre avec un léger délai entre chaque (j'utilise l'index)
+        Array.from(links).forEach((link, index) => {
+            setTimeout(() => {
+                link.style.opacity = "1"
+            }, 200 * index)
+        })
+    }
+
+
+})
