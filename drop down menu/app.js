1ea74d4d4e1207ffebfea819826da328d79ca260
djalabz <djalabz@gmail.com>
2025-01-29 14:25:30 +0100

    :lipstick: Burger menu exo added

---
+++ drop down menu/app.js
@@ -0,0 +1,12 @@
+// FAIRE UN DROP DOWN MENU AVEC JS (Il faudra modifier à la fois le HTML et le JS)
+
+// 1) Vous allez devoir insérer le burger icone dans le html 
+// 2) Dans le JS vous viendrez sélectionner le burger icone 
+// 3) Vous ajouterez un écouteur d'événement sur le burger icone pour le click
+// 4) Lors du click vous pourrez : 
+//  - Soit modifier du CSS pour créer cet effet de glissement (via les transitions ...)
+//  - Soit ajouter des classes de animate css 
+//  - Soit vous le faites sans animation mais fonctionnel
+
+// En revanche, un impératif, lorsque l'on clique une première fois le menu s'ouvre
+// Et lorsque l'on reclique il se ferme
